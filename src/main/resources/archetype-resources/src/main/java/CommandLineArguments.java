7f8bf6d0100e1f946909f1ce2a75d6d3e25d878b
thexman <thexman@dir.bg>
2022-04-20 04:08:52 +0300

    Initial implementation

---
+++ src/main/resources/archetype-resources/src/main/java/CommandLineArguments.java
@@ -0,0 +1,35 @@
+package $package;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.beust.jcommander.Parameter;
+import lombok.Builder;
+import lombok.Data;
+
+/**
+ * Hello world!
+ */
+@Builder(toBuilder = true)
+@Data
+public class CommandLineArguments {
+    @Builder.Default
+    @Parameter
+    private List<String> parameters = new ArrayList<>();
+
+    @Builder.Default
+    @Parameter(names = { "--number1", "-x" }, description = "Input number 1")
+    private Integer x = 1;
+
+    @Builder.Default
+    @Parameter(names = { "--number2", "-y" }, description = "Input number 2")
+    private Integer y = 1;
+
+    @Builder.Default
+    @Parameter(names = "--operation", description = "Math operation to perform")
+    private String operation = "add";
+
+    @Builder.Default
+    @Parameter(names = "--debug", description = "Debug mode")
+    private boolean debug = false;
+}
