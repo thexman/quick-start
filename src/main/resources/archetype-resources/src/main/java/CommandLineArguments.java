cd21fb26d1ccb3b3864d28ba1cbf7239fee660ff
Kiril Arabadzhiyski <thexman@dir.bg>
2022-05-21 01:31:04 +0300

    Updated dependencies to latest versions. Log4j2 async appenders and new pattern layout. Required parameters in CommandLineArguments

--- src/main/resources/archetype-resources/src/main/java/CommandLineArguments.java
+++ src/main/resources/archetype-resources/src/main/java/CommandLineArguments.java
@@ -18,15 +18,15 @@ public class CommandLineArguments {
     private List<String> parameters = new ArrayList<>();
 
     @Builder.Default
-    @Parameter(names = { "--number1", "-x" }, description = "Input number 1")
+    @Parameter(names = { "--number1", "-x" }, required = true, description = "Input number 1")
     private Integer x = 1;
 
     @Builder.Default
-    @Parameter(names = { "--number2", "-y" }, description = "Input number 2")
+    @Parameter(names = { "--number2", "-y" }, required = true, description = "Input number 2")
     private Integer y = 1;
 
     @Builder.Default
-    @Parameter(names = "--operation", description = "Math operation to perform")
+    @Parameter(names = "--operation", required = true, description = "Math operation to perform")
     private String operation = "add";
 
     @Builder.Default
